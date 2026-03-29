94f8d731c9284a95a70b55e44e7568fc8f826085
ryansmj <136880008+ryansmj@users.noreply.github.com>
2026-03-29 09:17:19 +0900

    주석변경

--- src/main/java/com/publicservice/controller/UserController.java
+++ src/main/java/com/publicservice/controller/UserController.java
@@ -66,19 +66,18 @@ public class UserController {
         return "user/join";
     }
 
-    //회원가입메핑추가
+    //아이디찾기추가
     @GetMapping("/findID")
     public String findID() {
         return "user/findID";
     }
 
-    //아이디찾기추가
+    //비밀번호찾기추가
     @GetMapping("/findPWD")
     public String findPWD() {
         return "user/findPWD";
     }
     
-    //비밀번호찾기추가
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
