8ef5e2a5cd496e85753d2c0ce5caa83a7d236d69
ryansmj <136880008+ryansmj@users.noreply.github.com>
2026-03-29 23:51:55 +0900

    +++컴퓨터저장소에맞추어서수정해야함+++ 이미지관련보이게추가

---
+++ src/main/java/com/publicservice/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.publicservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img_space/**")
+                .addResourceLocations("file:///F:/publicservice/img_space/");
+    }
+}
